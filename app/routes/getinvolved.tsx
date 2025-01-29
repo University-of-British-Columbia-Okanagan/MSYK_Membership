c19911c45f9fa8ed99b9e265cd5178ea5d273f17
Ishika Agarwal <ishikaagarwal@Ishikas-MacBook-Pro.local>
2025-01-28 17:47:55 -0800

    get involved section in progress

--- app/routes/getinvolved.tsx
+++ app/routes/getinvolved.tsx
@@ -1,10 +1,15 @@
+import HeroSection from "@/components/ui/Get Involved/HeroSection";
+import GallerySection from "@/components/ui/Get Involved/GallerySection";
+import Footer from "@/components/ui/Home/Footer";
+
 export default function GetInvolved() {
-    return (
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold mb-6">Get Involved</h1>
-        <p className="text-lg text-gray-700">Get Involved Page Content</p>
-      </div>
-    )
-  }
-  
-  
+  return (
+    <main>
+      <HeroSection />
+
+      <GallerySection />
+
+      <Footer />
+    </main>
+  );
+}
