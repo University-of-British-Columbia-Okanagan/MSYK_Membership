d85906bcbdd9e32f0f74dbcce078e820be121053
Ariq Muldi <ariqmuldi@gmail.com>
2025-02-05 16:45:36 -0800

    Added db.server.ts, session.server.ts, and singleton.server.ts

---
+++ app/utils/session.server.ts
@@ -0,0 +1,12 @@
+import { createCookieSessionStorage, redirect } from "react-router";
+import bcrypt from "bcryptjs";
+
+import { db } from "./db.server";
+
+export async function register() {
+    return;
+}
+
+// export async function test() {
+//   return { id: "test" };
+// }
