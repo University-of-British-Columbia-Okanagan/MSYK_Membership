ba930434d1a89bf6ee6b434178c4002dd16bebe9
Ariq Muldi <ariqmuldi@gmail.com>
2025-03-27 23:07:09 -0700

    added possible express code

--- entry.server.ts
+++ entry.server.ts
@@ -1,3 +1,12 @@
+// import express from "express";
 import { startMonthlyMembershipCheck } from "./app/models/membership.server";
+
+// const app = express();
+// const PORT = 3000;
+
 console.log("hello from entry.server.ts");
 startMonthlyMembershipCheck();
+
+// app.listen(PORT, () => {
+//     console.log(`Express server is running on port ${PORT}`);
+//   });
