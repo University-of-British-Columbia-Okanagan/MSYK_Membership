1df473724b2705cab2a61085f3fa32033f28e88a
Ariq Muldi <ariqmuldi@gmail.com>
2025-07-22 17:34:03 -0700

    Formatting in workshoplist.tsx

--- app/components/ui/Dashboard/workshoplist.tsx
+++ app/components/ui/Dashboard/workshoplist.tsx
@@ -21,18 +21,13 @@ export default function WorkshopList({
   workshops,
   isAdmin,
 }: WorkshopListProps) {
-
   return (
     <div className="p-6">
       <h2 className="text-3xl font-bold mb-6">{title}</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 auto-rows-fr">
         {workshops.map((workshop) => {
           return (
-            <WorkshopCard
-              key={workshop.id}
-              {...workshop}
-              isAdmin={isAdmin}
-            />
+            <WorkshopCard key={workshop.id} {...workshop} isAdmin={isAdmin} />
           );
         })}
       </div>
