e5edd42c6d3bdc37c30a0ec8f1ae7f617609080a
Anand <51914102+Jupiter-is-BIG@users.noreply.github.com>
2025-08-15 19:30:25 +0100

    158 UI components naming (#162)
    * updated naming
    
    * updated package lock
    
    * updated names
    
    ---------
    
    Co-authored-by: Anand <anand@Anand.local>

---
+++ app/components/ui/Dashboard/Workshoplist.tsx
@@ -0,0 +1,36 @@
+import WorkshopCard from "./workshopcard";
+
+interface Workshop {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  type: string;
+  occurrences: { id: number; startDate: string; endDate: string }[];
+  isRegistered: boolean;
+}
+
+interface WorkshopListProps {
+  title: string;
+  workshops: Workshop[];
+  isAdmin: boolean;
+}
+
+export default function WorkshopList({
+  title,
+  workshops,
+  isAdmin,
+}: WorkshopListProps) {
+  return (
+    <div className="p-6">
+      <h2 className="text-3xl font-bold mb-6">{title}</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 auto-rows-fr">
+        {workshops.map((workshop) => {
+          return (
+            <WorkshopCard key={workshop.id} {...workshop} isAdmin={isAdmin} />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
