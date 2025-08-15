b4431773737b2f87d15020b1dd65b867b1cbae69
Anand <anand@Anand.local>
2025-08-15 19:27:20 +0100

    updated names

---
+++ app/components/ui/Dashboard/Equipmentlist.tsx
@@ -0,0 +1,30 @@
+import EquipmentCard from "./equipmentcard";
+
+interface Equipment {
+  id: number;
+  name: string;
+  description: string;
+  availability: boolean;
+  status: "available" | "booked" | "unavailable";
+  imageUrl?: string;
+  bookingId?: number;
+}
+
+export default function EquipmentList({
+  equipments,
+  isAdmin,
+}: {
+  equipments: Equipment[];
+  isAdmin: boolean;
+}) {
+  return (
+    <div className="p-6">
+      <h2 className="text-3xl font-bold mb-6">Available Equipment</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 auto-rows-fr">
+        {equipments.map((equipment) => (
+          <EquipmentCard key={equipment.id} {...equipment} isAdmin={isAdmin} />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ app/components/ui/Dashboard/Sidebar.tsx
@@ -0,0 +1,125 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+import { Link } from "react-router-dom";
+import {
+  Home,
+  Calendar,
+  User,
+  LogOut,
+  ClipboardList,
+  Package,
+  BookOpen,
+  BugIcon,
+  Heart,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Form as RouterForm } from "react-router";
+
+export function AppSidebar() {
+  return (
+    <Sidebar className="h-screen w-64 bg-white shadow-lg flex flex-col">
+      {/* Sidebar Header with Logo */}
+      <SidebarHeader className="flex justify-center p-4">
+        <Link to="/dashboard/user">
+          <img
+            src="/images/Makerspace Horizontal Text Logo Colour-01.avif"
+            alt="Makerspace YK"
+            className="h-30"
+          />
+        </Link>
+      </SidebarHeader>
+
+      {/* Sidebar Content - Navigation Links */}
+      <SidebarContent className="flex flex-col gap-3 px-4 flex-grow space-y-4">
+        <SidebarGroup className="space-y-10">
+          <Link
+            to="/dashboard/profile"
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-500"
+          >
+            <User className="w-5 h-5" />
+            Profile
+          </Link>
+          <Link
+            to="/dashboard/myworkshops"
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-500"
+          >
+            <BookOpen className="w-5 h-5" />
+            My Workshops
+          </Link>
+          <Link
+            to="/dashboard/workshops"
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-500"
+          >
+            <ClipboardList className="w-5 h-5" />
+            All Workshops
+          </Link>
+          <Link
+            to="/dashboard/events"
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-500"
+          >
+            <Calendar className="w-5 h-5" />
+            Events
+          </Link>
+          <Link
+            to="/dashboard/equipments"
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-500"
+          >
+            <Package className="w-5 h-5" />
+            All Equipments
+          </Link>
+          <Link
+            to="/dashboard/myequipments"
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-500"
+          >
+            <Package className="w-5 h-5" />
+            My Equipments
+          </Link>
+          <Link
+            to="/dashboard/memberships"
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-500"
+          >
+            <Home className="w-5 h-5" />
+            Membership Plans
+          </Link>
+          <Link
+            to="/dashboard/volunteer"
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-500"
+          >
+            <Heart className="w-5 h-5" />
+            Volunteer
+          </Link>
+          <Link
+            to="/dashboard/report"
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-500"
+          >
+            <BugIcon className="w-5 h-5" />
+            Report Issue
+          </Link>
+        </SidebarGroup>
+      </SidebarContent>
+
+      {/* Sidebar Footer - Sign Out Button */}
+      <SidebarFooter className="p-4">
+        <Separator className="mb-4" />
+        <RouterForm action="/logout" method="post">
+          <Button
+            variant="secondary"
+            className="flex items-center gap-2 w-full"
+            type="submit"
+          >
+            <LogOut className="w-5 h-5" />
+            Sign Out
+          </Button>
+        </RouterForm>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
+
+export default AppSidebar;
