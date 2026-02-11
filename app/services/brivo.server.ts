3c14634c1e10da5dad25dbd0d443b199f7d83619
Ariq Muldi <ariqmuldi@gmail.com>
2026-02-11 11:41:09 -0800

    Added api-key header to Brivo OAuth token request

--- app/services/brivo.server.ts
+++ app/services/brivo.server.ts
@@ -105,6 +105,7 @@ class BrivoClient {
           Authorization: `Basic ${basicAuth}`,
           "Content-Type": "application/x-www-form-urlencoded",
           Accept: "application/json",
+          "api-key": this.env.apiKey ?? ""
         },
         body: new URLSearchParams({
           grant_type: "password",
