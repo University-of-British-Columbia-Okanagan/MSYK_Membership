9bd732ed5d3befd319a9e228ea8a45641c4e79d7
Ariq Muldi <ariqmuldi@gmail.com>
2026-08-23 15:09:27 -0700

    refactor: use Response.json in accesslogs loader

--- app/routes/dashboard/accesslogs.tsx
+++ app/routes/dashboard/accesslogs.tsx
@@ -1,5 +1,4 @@
 import type { LoaderFunctionArgs } from "react-router-dom";
-import { json } from "@remix-run/node";
 import { getAccessLogs } from "~/models/accessLog.server";
 import { Link, useLoaderData } from "react-router-dom";
 import { SidebarProvider } from "~/components/ui/sidebar";
@@ -40,7 +39,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
     endDate,
   });
 
-  return json({
+  return Response.json({
     logs,
     total,
     page,
